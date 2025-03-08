1caa2cf244ef69b525de467949af8a6aaf80809d
Your Name <you@example.com>
2025-03-08 10:01:16 +0000

    remve comment

--- week_05_part_2.py
+++ week_05_part_2.py
@@ -1,25 +1,3 @@
-
-
-
-# this is the one use 
-# import rclpy
-# from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
-# from turtlebot3occupancygrid import Turtlebot3OccupancyGrid
-
-# def main(*args, **kwargs):
-#     rclpy.init(*args, **kwargs)
-#     node = Turtlebot3OccupancyGrid(node_name="turtlebot3_occupancy_grid")
-#     # rclpy.spin(node)
-#     # node.destroy_node()
-#     # rclpy.try_shutdown()
-#     executor = MultiThreadedExecutor()
-#     try:
-#         rclpy.spin(node=node, executor=executor)
-#     except (KeyboardInterrupt, ExternalShutdownException):
-#         pass
-#     finally:
-#         rclpy.try_shutdown()
-
 import rclpy
 from myoccupancygrid import MyOccupancyGrid
 
